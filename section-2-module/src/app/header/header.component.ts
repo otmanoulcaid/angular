3cb19fd7a293444485844028f1af9d83a5135b65
otmanoulcaid <ooulcaid.1337@gmail.com>
2025-08-22 18:04:59 +0100

    essentials module based

---
+++ section-2-module/src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { AppModule } from '../app.module';
+
+@Component({
+  selector: 'app-header',
+  standalone: false,
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+  // imports: [AppModule]
+})
+export class HeaderComponent {}
---
+++ section-2-module/src/app/tasks/tasks.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
+import { TasksComponent } from "./tasks.component";
+import { SharedModule } from "../shared/shared.module";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+
+@NgModule({
+    declarations: [
+        TaskComponent,
+        NewTaskComponent,
+        TasksComponent
+    ],
+    exports: [
+        TasksComponent,
+    ],
+    imports: [SharedModule, CommonModule, FormsModule]
+})
+export class TasksModule {}
